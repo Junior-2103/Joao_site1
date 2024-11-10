a5d6dd6c788c30022d5070069c6429b19cdba45c
Junior-2103 <jjosewilker45@gmail.com>
2024-11-09 22:52:18 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+# streamlit run .\joao\streamlit_joao.py
+
+st.title(':green[Site sobre a minha rã touro]')
+
+st.subheader(':blue[a minha rã, é da especie Rã-touro, do nome cientifico (Lithobates castebeianus). Essa especie de rã pode atingir 20 centimetros de comprimento e pesar mais de 2kg, sendo a segunda maior especie de rã do mundo.]','left')
+
+botao = st.button('clique aqui para saber mais sobre a minha rã')
+
+
+
+if botao == True:
+    st.write('A minha rã virou rã no dia 21 de dezembro de 2022, pois antes dela virar realmente uma rã, ela era um girino, mas para ela virar rã, ocorreu um processo chamado de metamorfose. E ai esta uma foto dela quando era girino: ')
+    st.image(r'https://github.com/Junior-2103/Joao_site1/blob/main/IMG_20220720_200754.jpg')
+botao1 = st.button('ver foto atual')
+if botao1 == True:
+    st.write('essa e ela um pouco mais atual, mas oque você não sabe, é que na verdade, minha rã é macho e se chama Rana, eu pensei que era fêmea, por isso do nome, mas depois de um tempo descobri que na verdade era um macho, e decidi deixar o nome, ele tem 1 ano, o dia do aniversário dele é dia 21 de dezembro, pois nesse dia, foi o dia que ele passou de girino para Rã, se quizer saber mais, veja meu canal, se chama :frog word')
+    st.image(r'https://github.com/Junior-2103/Joao_site1/blob/main/IMG_20240225_184102.jpg')
